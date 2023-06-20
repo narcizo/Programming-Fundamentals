298b92f0bab6443a74dae206975c10c7959b3ba2
Narcizo Palioto <narcizo.gabriel2@gmail.com>
2023-06-20 19:21:01 -0300

    Added 'append' and 'pop' methods

--- python/linked_lists.py
+++ python/linked_lists.py
@@ -10,6 +10,54 @@ class LinkedList:
                 node.next = Node(data=node_data)
                 node = node.next
                 
+    def append(self, data):
+        if self.head is None:
+            self.head = Node(data)
+            return
+        
+        for current_node in self:
+            pass
+        
+        current_node.next = Node(data)
+
+        return current_node.next     
+    
+    def add_first(self, data):
+        #TODO add node in the beggining of the list
+        ...
+        
+    def add_last(self, data):
+        #TODO add node in the end of the list
+        ...
+        
+    def add_after(self, target_node, data):
+        #TODO add node after target node
+        ...
+    
+    def add_before(self, target_node, data):
+        #TODO add node before target node
+        ...
+        
+    def remove(self, target_node_data):
+        #TODO removes first, middle or last nodes
+        ...       
+                
+    def pops(self):
+        if self.head is None:
+            raise Exception("Cannot remove element from empty list.")
+        
+        current_head = self.head
+        last_node = None
+        
+        node = current_head
+        while node.next is not None:
+            last_node = node
+            node = node.next
+        
+        last_node.next = None
+        
+        return node
+        
     def __iter__(self):
         node = self.head
         while node is not None:
@@ -38,21 +86,30 @@ class Node:
     def __str__(self):
         return str(self.data)
     
+    def __del__(self):
+        self.data = None
+        self.next = None
+    
 def main():
-    llist = LinkedList()
+    llist = LinkedList(['a', 'b', 'c', 'd', 'e', 'f'])
+    print(llist)
+    
+    llist.append('z')
     
     print(llist)
     
-    first_node = Node("a")
-    llist.head = first_node
+    print(llist.head)
     
-    sec_node = Node(1)
-    first_node.next = sec_node
+    llist.pops()
     
     print(llist)
     
-    llist2 = LinkedList([1, 2, 3, 4, 5,10])
-    print(llist2)
+    llist.append('z')
+    
+    print(llist)
+    
+    llist2 = LinkedList()
+    llist2.pops()
     
 if __name__ == "__main__":
     main()
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,8 @@
+import pytest
 import sys
+
 sys.path.insert(1, '/home/narcizo/Projects/Programming-Fundamentals/python')
+
 import linked_lists
 
 def test_create_empty_list():
@@ -28,3 +31,55 @@ def test_iterate_through_list():
                                       if type(el) is not str 
                                       else el 
                                       for el in list])
+    
+def test_append_element():
+    llist = linked_lists.LinkedList(['q', 'c', 'a', 'x'])
+    
+    list = []
+    for el in llist:
+        list.append(el)
+    
+    llist.append('a')
+    
+    list.append('a')
+    list.append('None')
+    
+    assert str(llist) == ' -> '.join([el.data 
+                                      if type(el) is not str 
+                                      else el 
+                                      for el in list])
+    
+def test_append_element_empty_list():
+    llist = linked_lists.LinkedList()
+    
+    llist.append('a')
+
+    assert str(llist) == 'a -> None'
+    
+    
+def test_pop_element():
+    llist = linked_lists.LinkedList(['q', 'c', 'a', 'x'])
+    
+    list = []
+    for el in llist:
+        list.append(el)
+    
+    llist.pops()
+    
+    list.pop()
+    list.append('None')
+    
+    assert str(llist) == ' -> '.join([el.data 
+                                      if type(el) is not str 
+                                      else el 
+                                      for el in list])
+    
+def test_pop_element_empty_list():
+    llist = linked_lists.LinkedList()
+    
+    with pytest.raises(Exception):
+        llist.pops()
+    
+    assert llist is not None
+    assert llist.head is None
+    
